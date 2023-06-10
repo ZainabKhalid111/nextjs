f339568baccf11f5c669f5dd7788b3e0c72eab29
ZainabKhalid111 <zainabkhalid3525@gmail.com>
2023-06-10 22:13:30 +0500

    first commit

---
+++ components/Nav.js
@@ -0,0 +1,15 @@
+// import Link from "next/link";
+
+
+const Nav = () => {
+    return (
+        <nav>
+            <ul>
+                <li>link</li>
+                <li></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Nav
---
+++ pages/contact.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const contact = () => {
+    return (
+        <div>contact Us page</div>
+    )
+}
+
+export default contact
